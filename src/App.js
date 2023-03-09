eebb948a9d096981e5cc0ceba007e74f02bdf633
Shawn Kok <shawnkok00@gmail.com>
2023-03-10 03:01:28 +0800

    Fixed bug crash

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import Home from './components/Home'
 import About from './components/About'
-import Height from './components/Height'
-import HeightComparison from './components/HeightComparison'
 
 const App = () => {
   return (
