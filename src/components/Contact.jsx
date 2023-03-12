f3148720bd588af71ffa969587aeb5e42bae3458
Shawn Kok <shawnkok00@gmail.com>
2023-03-12 20:26:24 +0800

    Implemented getforms for contact segment

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -10,7 +10,7 @@ const Contact = () => {
                 
             </div>
             <div className="flex justify-center">
-                <form method='POST' action="https://getform.io/f/a699a1b2-f225-434e-b317-1fbbde8e006c" className='flex flex-col max-w-[600px] w-full'>
+                <form method='POST' action="https://getform.io/f/e3fcfe93-80e5-4a72-af51-fc41314b7c13" className='flex flex-col max-w-[600px] w-full'>
                     <div className="flex flex-row items-center mt-4">
                         <FaMagic size={36} className='hidden sm:block mr-8 text-[#4169e1]'/>
                         <p className='py-4'>Let's make some magic happen! Drop me a message and let's brew up some enchanting solutions together.</p>
@@ -18,7 +18,7 @@ const Contact = () => {
                     <input className='bg-[#e3e8ef] p-2' type="text" placeholder='Name' name='name' />
                     <input className='my-4 p-2 bg-[#e3e8ef]' type="email" placeholder='Email' name='email' />
                     <textarea className='bg-[#e3e8ef] p-2' name="message" rows="10" placeholder='Message'></textarea>
-                    <button className='rounded-2xl border-2 hover:bg-secondary hover:text-white px-4 py-3 my-8 mx-auto flex items-center'>Let's Collaborate</button>
+                    <button className='flex items-center justify-center font-bold rounded-2xl border-2 hover:bg-secondary hover:text-white px-4 py-3 my-8 mx-auto flex items-center w-40'>HMU</button>
                 </form>
             </div>
         </div>
